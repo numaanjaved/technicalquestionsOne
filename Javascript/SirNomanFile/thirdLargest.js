664c17576bbc4aabfa939046f1f2da7a63c0d6e7
Ahmed Tahiri <ahmedtahiri.webdev@gmail.com>
2024-10-27 11:38:20 +0500

    More optimized solution

--- Javascript/SirNomanFile/thirdLargest.js
+++ Javascript/SirNomanFile/thirdLargest.js
@@ -32,4 +32,35 @@ let arrValues = (arrayOfNum) => {
     return [thirdLargest, lastLargest];
 }
 let arrayOfNum = [3, 5, 9, 10, 2, 15, 50, 20, 30];
-console.log(arrValues(arrayOfNum));
+// console.log(arrValues(arrayOfNum));
+
+
+
+// solution 2:
+
+
+let findArray = (arr) => {
+    let firstLargest = arr[0], secondLargest = arr[0], thirdLargest = arr[0], lastLargest = arr[0];
+    arr.forEach((num) => {
+        if (num > firstLargest) {
+            thirdLargest = secondLargest;
+            secondLargest = firstLargest;
+            firstLargest = num;
+        } else if (num > secondLargest && num < firstLargest) {
+            thirdLargest = secondLargest;
+            secondLargest = num;
+        } else if (num > thirdLargest && num < secondLargest) { thirdLargest = num; }
+        if (num < lastLargest) { lastLargest = num; }
+    });
+    if (firstLargest > (secondLargest * 1.2)) { return [thirdLargest, lastLargest]; } else { return 0; }
+    // console.log(`The Largest Number in the Array is ${firstLargest}`);
+    // console.log(`The Second Largest Number in the Array is ${secondLargest}`);
+    // console.log(`The Third Largest Number in the Array is ${thirdLargest}`);
+    // console.log(`The Last Largest Number(smallest) in the Array is ${lastLargest}`);
+}
+let result = findArray(arrayOfNum)
+if (result !== 0) {
+    console.log(result);
+} else {
+    console.log(`The first Largest number is not 20% greater than second largest`)
+}
